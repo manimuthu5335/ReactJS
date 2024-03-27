22661b72c2e8375aaaa0085b9ef9011b1620be38
Manimuthu <157777529+manimuthu5335@users.noreply.github.com>
2024-03-27 17:50:58 +0530

    Add files via upload

---
+++ src/comp/footer.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import './footer.css'
+import { FaPiggyBank, FaShippingFast, FaHeadphonesAlt, FaWallet} from 'react-icons/fa';
+const Footer = () => {
+  return (
+    <>
+    <div className='footer'>
+        <div className='container'>
+            <div className='left-box'>
+                <div className='box'>
+                    <div className='icon_box'>
+                        <FaPiggyBank />
+                    </div>
+                    <div className='detail'>
+                        <h3>Great Saving</h3>
+                        <p>Save Money on Online Shopping in India.</p>
+                    </div>
+                </div>
+                    <div className='box'>
+                    <div className='icon_box'>
+                        <FaShippingFast />
+                    </div>
+                    <div className='detail'>
+                        <h3>Free Delivery</h3>
+                        <p>customers don't have to pay an ship fee.</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <div className='icon_box'>
+                        <FaHeadphonesAlt />
+                    </div>
+                    <div className='detail'>
+                        <h3>24X7 Support</h3>
+                        <p>Customers can get help and find answers to questions as soon as.</p>
+                    </div>
+                </div>
+                <div className='box'>
+                    <div className='icon_box'>
+                        <FaWallet />
+                    </div>
+                    <div className='detail'>
+                        <h3>Money Back</h3>
+                        <p>Customer is unhappy with a product or service, we will return.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='right_box'>
+                <div className='header'>
+                    {/* <img src='image/logo.webp' alt=''></img> */}
+                    <img src='image/fart.png' alt='logo'></img>
+                    <h2>ALPHA</h2>
+                    <p>The Alpha Group is one of India's leading digital commerce entities and includes group companies Alpha, Myntra, Alpha Wholesale, Alpha Health+, Cleartrip and ANS Commerce.</p>
+                </div>
+                <div className='bottom'>
+                    <div className='box'>
+                        <h3>Your Account</h3>
+                        <ul>
+                            <li>About us</li>
+                            <li>Account</li>
+                            <li>Payment</li>
+                            <li>sales</li>
+                        </ul>
+                    </div>
+                    <div className='box'>
+                        <h3>products</h3>
+                        <ul>
+                            <li>Delivery</li>
+                            <li>Track Oder</li>
+                            <li>New product</li>
+                            <li>old product</li>
+                        </ul>
+                    </div>
+                    <div className='box'>
+                        <h3>contact us</h3>
+                        <ul>
+                            <li>3/9,Velachery,Chennai,India.</li>
+                            <li>+91 9629538588</li>
+                            <li>alphapvt@gmail.com</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Footer
